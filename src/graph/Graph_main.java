64fc9a1633aafd6237de392d4d6007886103ea6e
sho <sho@10.4.154.68>
2018-01-08 20:16:36 +0900

    graph

---
+++ src/graph/Graph_main.java
@@ -0,0 +1,14 @@
+package graph;
+
+public class Graph_main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Graph_view frame = new Graph_view();
+		frame.setBounds(5,5,655,455);
+		frame.setVisible(true);
+		
+
+	}
+
+}
